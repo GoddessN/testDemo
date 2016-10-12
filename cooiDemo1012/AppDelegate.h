20d3b5c709b12467f9faf038e11c1cd1f3bc3bb0
niuda <luolita90@icloud.com>
2016-10-12 14:41:54 +0800

    start prj!

---
+++ cooiDemo1012/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cooiDemo1012
+//
+//  Created by niuda on 16/10/12.
+//  Copyright © 2016年 cooi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cooiDemo1012/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cooiDemo1012
+//
+//  Created by niuda on 16/10/12.
+//  Copyright © 2016年 cooi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
